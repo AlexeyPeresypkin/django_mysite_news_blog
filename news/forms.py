22f8d7183be12425300fb144155eb1013f6df9d1
Alexey Peresypkin <aepre3@gmail.com>
2021-09-15 07:15:20 +0300

    add register form, pagination

--- news/forms.py
+++ news/forms.py
@@ -1,8 +1,29 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
 from news.models import News
 import re
+
+
+class UserRegisterForm(UserCreationForm):
+    username = forms.CharField(label='Имя пользователя', help_text='Подсказка #1',
+                               widget=forms.TextInput(
+                                   attrs={'class': 'form-control'}))
+    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(
+        attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label='Подтверждение пароля',
+                                widget=forms.PasswordInput(
+                                    attrs={'class': 'form-control'}))
+
+    email = forms.EmailField(label='Почта',
+                             widget=forms.EmailInput(
+                                 attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
 
 
 class NewsForm(forms.ModelForm):
